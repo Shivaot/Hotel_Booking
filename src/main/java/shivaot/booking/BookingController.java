4a0b20c4535ac44a06aecf17f640ce1422ad4d83
Shivaot <shivatiwari757@gmail.com>
2019-09-09 01:18:02 +0530

    JSON doc added.

--- src/main/java/shivaot/booking/BookingController.java
+++ src/main/java/shivaot/booking/BookingController.java
@@ -1,11 +1,16 @@
 package shivaot.booking;
 
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.pojo.ApiStage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/bookings")
+@Api(name = "Hotel Booking API",description = "methods for managing hotel bookings",stage = ApiStage.RC)
 public class BookingController {
     private BookingRepository bookingRepository;
 
@@ -15,23 +20,27 @@ public class BookingController {
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ApiMethod(description = "Get all hotel bookings from database")
     public List<HotelBooking> getAll(){
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/affordable/{price}" , method = RequestMethod.GET)
-    public List<HotelBooking> getAffordableHotel(@PathVariable double price){
+    @ApiMethod(description = "Get affordable hotels by pricePerNight as provided")
+    public List<HotelBooking> getAffordableHotel(@ApiPathParam(name = "price") @PathVariable double price){
         return bookingRepository.findByPricePerNightLessThan(price);
     }
 
     @RequestMapping(value = "/create" , method = RequestMethod.POST)
+    @ApiMethod(description = "Create a new hotel and save it in database")
     public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking){
         bookingRepository.save(hotelBooking);
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/delete/{id}" , method = RequestMethod.POST)
-    public List<HotelBooking> remove(@PathVariable long id){
+    @ApiMethod(description = "Remove a hotel from database by provided id")
+    public List<HotelBooking> remove(@ApiPathParam(name = "id") @PathVariable long id){
         bookingRepository.deleteById(id);
         return bookingRepository.findAll();
     }
--- src/main/java/shivaot/booking/ViewController.java
+++ src/main/java/shivaot/booking/ViewController.java
@@ -1,6 +1,7 @@
 package shivaot.booking;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,8 +23,8 @@ public class ViewController {
     public String index(Model model){
 
         model.addAttribute("datetime",new Date());
-        model.addAttribute("username","Shiva Tiwari");
-        model.addAttribute("mode","production");
+        model.addAttribute("username","Shiva");
+        model.addAttribute("mode",appMode);
 
         return "index";
     }
